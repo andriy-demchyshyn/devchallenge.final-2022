f2662b491b4f4e3f777e406d823cf014009fefea
Andriy Demchyshyn <andrii.demchyshyn@gmail.com>
2022-11-17 17:25:55 +0200

    Updated ImageController

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -11,11 +11,12 @@ class ImageController extends Controller
      * Process image and detect darkest cells
      * 
      * @param  \App\Http\Requests\ProcessImageRequest  $request
+     * @param  \App\Services\ImageHandler  $image_handler
      * @return \Illuminate\Http\Response
      */
-    public function process(ProcessImageRequest $request)
+    public function process(ProcessImageRequest $request, ImageHandler $image_handler)
     {
-        $image_handler = new ImageHandler($request->image);
+        $image_handler->processImage($request->image);
         $mines = $image_handler->filterDarkestCells($request->min_level);
         return response()->json(['mines' => $mines], 200);
     }
